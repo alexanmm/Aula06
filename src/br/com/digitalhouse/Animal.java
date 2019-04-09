b94b4999a72f81ac7c5a21e630f8e027abbb54ac
alexanmm <alexandre.moreira.medeiros@gmail.com>
2019-04-08 21:46:14 -0300

    Primeiro ajuste

--- src/br/com/digitalhouse/Animal.java
+++ src/br/com/digitalhouse/Animal.java
@@ -1,4 +1,38 @@
 package br.com.digitalhouse;
 
-public class Animal {
+public abstract class Animal {
+
+    private String tamanho;
+    private String raca;
+
+    //Construtor
+    public Animal(){
+
+    }
+
+    public Animal(String novoTamanho, String novaRaca){
+
+        tamanho = novoTamanho;
+        raca = novaRaca;
+    }
+
+    //Getter and Setter
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public abstract boolean ehMamifero();
+
 }
--- src/br/com/digitalhouse/Main.java
+++ src/br/com/digitalhouse/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Cachorro cachorro = new Cachorro("Bidu", "20", "Vira-Lata");
+
+        System.out.println("Esse cachorro é mamífero? " + cachorro.ehMamifero());
+
+
+
     }
 
 }
